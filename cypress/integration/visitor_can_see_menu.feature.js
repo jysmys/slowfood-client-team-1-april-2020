17cdb17def7e2345c31c9e874e9b77461f104808
pauline_barnades <barnades.pauline@gmail.com>
2020-05-05 11:41:59 +0200

    Writting test for what usser can see on menu page

--- cypress/integration/visitor_can_see_menu.feature.js
+++ cypress/integration/visitor_can_see_menu.feature.js
@@ -1,5 +1,13 @@
 describe('User can see menu', () => {
   it('user can see menu page', () => {
     cy.visit('http://localhost:3001/')
+
+    cy.get('div#starters').should('be.visible')
+    cy.get('div#main_course').should('be.visible')
+    cy.get('button#add').should('be.visible')
+    cy.get('button#minus').should('be.visible')
+    cy.get('div#subtotal').should('be.visible')
+    cy.get('button#plus').should('be.visible')
+
   })
 } )
